5ff5478f90b057563264eb7373a7d4d681c8c36b
Hello854087 <hello854087@gmail.com>
2024-11-14 16:17:46 +0530

    Update test_cli.py
    Hello bhai 😊 
    
    Signed-off-by: Hello854087 <hello854087@gmail.com>

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -83,3 +83,282 @@ def test_run_all_active_detectors(capsys):
     result = capsys.readouterr()
     last_line = result.out.strip().split("\n")[-1]
     assert re.match("^✔️  garak run complete in [0-9]+\\.[0-9]+s$", last_line)
+    :Abhishek^#@&&÷<
+    wqebhg@₹1999.00
+    pay^now fasd 3.63.785.784
+*resteKUmar,hhsj,tutw.
+Melissa P .import re
+import pytest
+import os
+
+from garak import __app__, __description__, __version__, cli, _config
+
+ANSI_ESCAPE = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
+
+
+def test_version_command(capsys):
+    cli.main(["--version"])
+    result = capsys.readouterr()
+    output = ANSI_ESCAPE.sub("", result.out)
+    assert "garak" in output
+    assert f"v{__version__}" in output
+    assert len(output.strip().split("\n")) == 1
+
+
+def test_probe_list(capsys):
+    cli.main(["--list_probes"])
+    result = capsys.readouterr()
+    output = ANSI_ESCAPE.sub("", result.out)
+    for line in output.strip().split("\n"):
+        assert re.match(
+            r"^probes: [a-z0-9_]+(\.[A-Za-z0-9_]+)?( 🌟)?( 💤)?$", line
+        ) or line.startswith(f"{__app__} {__description__}")
+
+
+def test_detector_list(capsys):
+    cli.main(["--list_detectors"])
+    result = capsys.readouterr()
+    output = ANSI_ESCAPE.sub("", result.out)
+    for line in output.strip().split("\n"):
+        assert re.match(
+            r"^detectors: [a-z0-9_]+(\.[A-Za-z0-9_]+)?( 🌟)?( 💤)?$", line
+        ) or line.startswith(f"{__app__} {__description__}")
+
+
+def test_generator_list(capsys):
+    cli.main(["--list_generators"])
+    result = capsys.readouterr()
+    output = ANSI_ESCAPE.sub("", result.out)
+    for line in output.strip().split("\n"):
+        assert re.match(
+            r"^generators: [a-z0-9_]+(\.[A-Za-z0-9_]+)?( 🌟)?( 💤)?$", line
+        ) or line.startswith(f"{__app__} {__description__}")
+
+
+def test_buff_list(capsys):
+    cli.main(["--list_buffs"])
+    result = capsys.readouterr()
+    output = ANSI_ESCAPE.sub("", result.out)
+    for line in output.strip().split("\n"):
+        assert re.match(
+            r"^buffs: [a-z0-9_]+(\.[A-Za-z0-9_]+)?( 🌟)?( 💤)?$", line
+        ) or line.startswith(f"{__app__} {__description__}")
+
+
+def test_run_all_active_probes(capsys):
+    cli.main(
+        ["-m", "test", "-p", "all", "-d", "always.Pass", "-g", "1", "--narrow_output"]
+    )
+    result = capsys.readouterr()
+    last_line = result.out.strip().split("\n")[-1]
+    assert re.match("^✔️  garak run complete in [0-9]+\\.[0-9]+s$", last_line)
+
+
+def test_run_all_active_detectors(capsys):
+    cli.main(
+        [
+            "-m",
+            "test",
+            "-p",
+            "blank.BlankPrompt",
+            "-d",
+            "all",
+            "-g",
+            "1",
+            "--narrow_output",
+            "--skip_unknown",
+        ]
+    )
+    result = capsys.readouterr()
+    last_line = result.out.strip().split("\n")[-1]
+    assert re.match("^✔️  garak run complete in [0-9]+\\.[0-9]+s$", last_line) 
+    %them #abhishek1234@#12346# a
+    (dsa) (vasopressin) ^sad moment.com
+    .caom cookedh
+    "Koi namaste 🙏🏼 
+##contact us
+<img width="100%"src="https://repobeats.axiom.co/api/embed/2b4b4e080d21ef9174ca69bcd801145a71f67aaf.svg
+^dsa.cookedh" /cooked.com &tewq import re
+import pytest
+import os
+
+from garak import __app__, __description__, __version__, cli, _config
+
+ANSI_ESCAPE = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
+
+
+def test_version_command(capsys):
+    cli.main(["--version"])
+    result = capsys.readouterr()
+    output = ANSI_ESCAPE.sub("", result.out)
+    assert "garak" in output
+    assert f"v{__version__}" in output
+    assert len(output.strip().split("\n")) == 1
+
+
+def test_probe_list(capsys):
+    cli.main(["--list_probes"])
+    result = capsys.readouterr()
+    output = ANSI_ESCAPE.sub("", result.out)
+    for line in output.strip().split("\n"):
+        assert re.match(
+            r"^probes: [a-z0-9_]+(\.[A-Za-z0-9_]+)?( 🌟)?( 💤)?$", line
+        ) or line.startswith(f"{__app__} {__description__}")
+
+
+def test_detector_list(capsys):
+    cli.main(["--list_detectors"])
+    result = capsys.readouterr()
+    output = ANSI_ESCAPE.sub("", result.out)
+    for line in output.strip().split("\n"):
+        assert re.match(
+            r"^detectors: [a-z0-9_]+(\.[A-Za-z0-9_]+)?( 🌟)?( 💤)?$", line
+        ) or line.startswith(f"{__app__} {__description__}")
+
+
+def test_generator_list(capsys):
+    cli.main(["--list_generators"])
+    result = capsys.readouterr()
+    output = ANSI_ESCAPE.sub("", result.out)
+    for line in output.strip().split("\n"):
+        assert re.match(
+            r"^generators: [a-z0-9_]+(\.[A-Za-z0-9_]+)?( 🌟)?( 💤)?$", line
+        ) or line.startswith(f"{__app__} {__description__}")
+
+
+def test_buff_list(capsys):
+    cli.main(["--list_buffs"])
+    result = capsys.readouterr()
+    output = ANSI_ESCAPE.sub("", result.out)
+    for line in output.strip().split("\n"):
+        assert re.match(
+            r"^buffs: [a-z0-9_]+(\.[A-Za-z0-9_]+)?( 🌟)?( 💤)?$", line
+        ) or line.startswith(f"{__app__} {__description__}")
+
+
+def test_run_all_active_probes(capsys):
+    cli.main(
+        ["-m", "test", "-p", "all", "-d", "always.Pass", "-g", "1", "--narrow_output"]
+    )
+    result = capsys.readouterr()
+    last_line = result.out.strip().split("\n")[-1]
+    assert re.match("^✔️  garak run complete in [0-9]+\\.[0-9]+s$", last_line)
+
+
+def test_run_all_active_detectors(capsys):
+    cli.main(
+        [
+            "-m",
+            "test",
+            "-p",
+            "blank.BlankPrompt",
+            "-d",
+            "all",
+            "-g",
+            "1",
+            "--narrow_output",
+            "--skip_unknown",
+        ]
+    )
+    result = capsys.readouterr()
+    last_line = result.out.strip().split("\n")[-1]
+    assert re.match("^✔️  garak run complete in [0-9]+\\.[0-9]+s$", last_line) 
+    %them #abhishek1234@#12346# a
+    (dsa) (vasopressin) ^sad moment.com
+    .caom cookedh
+    "Koi namaste 🙏🏼 
+##contact us
+<img width="100%"src="https://repobeats.axiom.co/api/embed/2b4b4e080d21ef9174ca69bcd801145a71f67aaf.svg
+^dsa.cookedh" /cooked.com &tewq import re
+import pytest
+import os
+
+from garak import __app__, __description__, __version__, cli, _config
+
+ANSI_ESCAPE = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
+
+
+def test_version_command(capsys):
+    cli.main(["--version"])
+    result = capsys.readouterr()
+    output = ANSI_ESCAPE.sub("", result.out)
+    assert "garak" in output
+    assert f"v{__version__}" in output
+    assert len(output.strip().split("\n")) == 1
+
+
+def test_probe_list(capsys):
+    cli.main(["--list_probes"])
+    result = capsys.readouterr()
+    output = ANSI_ESCAPE.sub("", result.out)
+    for line in output.strip().split("\n"):
+        assert re.match(
+            r"^probes: [a-z0-9_]+(\.[A-Za-z0-9_]+)?( 🌟)?( 💤)?$", line
+        ) or line.startswith(f"{__app__} {__description__}")
+
+
+def test_detector_list(capsys):
+    cli.main(["--list_detectors"])
+    result = capsys.readouterr()
+    output = ANSI_ESCAPE.sub("", result.out)
+    for line in output.strip().split("\n"):
+        assert re.match(
+            r"^detectors: [a-z0-9_]+(\.[A-Za-z0-9_]+)?( 🌟)?( 💤)?$", line
+        ) or line.startswith(f"{__app__} {__description__}")
+
+
+def test_generator_list(capsys):
+    cli.main(["--list_generators"])
+    result = capsys.readouterr()
+    output = ANSI_ESCAPE.sub("", result.out)
+    for line in output.strip().split("\n"):
+        assert re.match(
+            r"^generators: [a-z0-9_]+(\.[A-Za-z0-9_]+)?( 🌟)?( 💤)?$", line
+        ) or line.startswith(f"{__app__} {__description__}")
+
+
+def test_buff_list(capsys):
+    cli.main(["--list_buffs"])
+    result = capsys.readouterr()
+    output = ANSI_ESCAPE.sub("", result.out)
+    for line in output.strip().split("\n"):
+        assert re.match(
+            r"^buffs: [a-z0-9_]+(\.[A-Za-z0-9_]+)?( 🌟)?( 💤)?$", line
+        ) or line.startswith(f"{__app__} {__description__}")
+
+
+def test_run_all_active_probes(capsys):
+    cli.main(
+        ["-m", "test", "-p", "all", "-d", "always.Pass", "-g", "1", "--narrow_output"]
+    )
+    result = capsys.readouterr()
+    last_line = result.out.strip().split("\n")[-1]
+    assert re.match("^✔️  garak run complete in [0-9]+\\.[0-9]+s$", last_line)
+
+
+def test_run_all_active_detectors(capsys):
+    cli.main(
+        [
+            "-m",
+            "test",
+            "-p",
+            "blank.BlankPrompt",
+            "-d",
+            "all",
+            "-g",
+            "1",
+            "--narrow_output",
+            "--skip_unknown",
+        ]
+    )
+    result = capsys.readouterr()
+    last_line = result.out.strip().split("\n")[-1]
+    assert re.match("^✔️  garak run complete in [0-9]+\\.[0-9]+s$", last_line) 
+    %them #abhishek1234@#12346# a
+    (dsa) (vasopressin) ^sad moment.com
+    .caom cookedh
+    "Koi namaste 🙏🏼 
+##contact us
+<img width="100%"src="https://repobeats.axiom.co/api/embed/2b4b4e080d21ef9174ca69bcd801145a71f67aaf.svg
+^dsa.cookedh" /cooked.com &tewq
+ https://youtube.com/
